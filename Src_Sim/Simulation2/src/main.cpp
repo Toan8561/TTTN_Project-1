618435314ce7f1904c9ab4a2ea78bc91c3c4839b
Toan8561 <nguyenphuc2001toan@gmail.com>
2025-06-24 01:21:12 +0700

    "add Simulation libraries"

--- Src_Sim/Simulation2/src/main.cpp
+++ Src_Sim/Simulation2/src/main.cpp
@@ -1,68 +1,54 @@
 #include <Arduino.h>
 
-#include "Wire.h"
-#include "SHT2x.h"
-
-SHT2x sht;
-
-
-//  TIMING SCHEDULE
-uint32_t lastTemp = 0;
-uint32_t intervalTemp = 2000;
-uint32_t lastHum = 0;
-uint32_t intervalHum = 5000;
-
-
-void setup()
-{
+#include <EtherCard.h>
+#define STATIC 0  // set to 1 to disable DHCP (adjust myip/gwip values below)
+#if STATIC
+// ethernet interface ip address
+static byte myip[] = { 192,168,1,200 };
+// gateway ip address
+static byte gwip[] = { 192,168,1,1 };
+#endif
+// ethernet mac address - must be unique on your network
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
+const char page[] PROGMEM =
+"HTTP/1.0 503 Service Unavailable\r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+  "<head><title>"
+    "Service Temporarily Unavailable"
+  "</title></head>"
+  "<body>"
+    "<h3>This service is currently unavailable</h3>"
+    "<p><em>"
+      "The main server is currently off-line.<br />"
+      "Please try again later."
+    "</em></p>"
+  "</body>"
+"</html>"
+;
+void setup(){
   Serial.begin(57600);
-  Serial.println();
-  Serial.println(__FILE__);
-  Serial.print("SHT2x_LIB_VERSION: \t");
-  Serial.println(SHT2x_LIB_VERSION);
-  Serial.println();
-
-  Wire.begin();
-  sht.begin();
-
-  uint8_t stat = sht.getStatus();
-  Serial.print(stat, HEX);
-  Serial.println();
-
-  sht.requestTemperature();
+  Serial.println("\n[backSoon]");
+  // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0)
+    Serial.println( "Failed to access Ethernet controller");
+#if STATIC
+  ether.staticSetup(myip, gwip);
+#else
+  if (!ether.dhcpSetup())
+    Serial.println("DHCP failed");
+#endif
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
 }
-
-
-void loop()
-{
-  uint32_t now = millis();
-
-  //  schedule and handle temperature
-  if ((now - lastTemp > intervalTemp) && (sht.getRequestType() == 0x00))
-  {
-    sht.requestTemperature();
+void loop(){
+  // wait for an incoming TCP packet, but ignore its contents
+  if (ether.packetLoop(ether.packetReceive())) {
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
   }
-  if (sht.reqTempReady())
-  {
-    lastTemp = now;
-    sht.readTemperature();
-    Serial.print("TEMP:\t");
-    Serial.println(sht.getTemperature(), 1);
-  }
-
-  //  schedule and handle humidity
-  if ((now - lastHum > intervalHum) && (sht.getRequestType() == 0x00))
-  {
-    sht.requestHumidity();
-  }
-  if (sht.reqHumReady())
-  {
-    lastTemp = now;
-    sht.readHumidity();
-    Serial.print("HUMI:\t");
-    Serial.println(sht.getHumidity(), 1);
-  }
-
-  //  do other things here
-  delay(1000);
 }
