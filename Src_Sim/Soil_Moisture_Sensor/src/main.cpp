475ae5fb0f3f6db25c7b9918da2a60a680a8d022
Toan8561 <nguyenphuc2001toan@gmail.com>
2025-07-04 07:18:18 +0700

    add simulation picture

--- Src_Sim/Soil_Moisture_Sensor/src/main.cpp
+++ Src_Sim/Soil_Moisture_Sensor/src/main.cpp
@@ -1,41 +1,57 @@
-/*
-VDD < --- > +5V
-Pin A0 < --- > A0 Soil sensor 1
-Pin A1 < --- > A0 Soil sensor 2,
-Pin A2 < --- > A0 Rain sensor
-Pin A3 < --- > A0 Light Sensor
-GND < --- > GND
-*/
 #include <Arduino.h>
 
-int sensorValue[4] = {0}; /*Soil sensor 1, Soil sensor 2, Rain sensor, Light Sensor*/
-float voltage[4] = {0};
-void setup() {
+#include <Adafruit_ADS1X15.h>
+
+//Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+
+void setup(void)
+{
   Serial.begin(57600);
-  for(int i=2; i<=5; i++){
-    pinMode(i, INPUT);
+  Serial.println("Hello!");
+
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
   }
 }
 
-void loop() {
-  for(int i=0; i<=3; i++){
-    sensorValue[i] = analogRead(i);
-    voltage[i] = (sensorValue[i] * 3.3) / 1023;
+void loop(void)
+{
+  int16_t adc0, adc1, adc2, adc3;
+  float volts0, volts1, volts2, volts3;
 
-  }
-  Serial.println("-----------------------------------");
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
 
-  Serial.print("Soil sensor 1"); Serial.print(" \t Digital Value: "); Serial.print(sensorValue[0]); 
-  Serial.print("    \t Analog Value: "); Serial.println(voltage[0]);
+  volts0 = ads.computeVolts(adc0);
+  volts1 = ads.computeVolts(adc1);
+  volts2 = ads.computeVolts(adc2);
+  volts3 = ads.computeVolts(adc3);
 
-  Serial.print("Soil sensor 2"); Serial.print(" \t Digital Value: "); Serial.print(sensorValue[1]); 
-  Serial.print("    \t Analog Value: "); Serial.println(voltage[1]);
+  Serial.println("-----------------------------------------------------------");
+  Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
+  Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
+  Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
+  Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(volts3); Serial.println("V");
 
-  Serial.print("Rain sensor  "); Serial.print(" \t Digital Value: "); Serial.print(sensorValue[2]); 
-  Serial.print("    \t Analog Value: "); Serial.println(voltage[2]);
-
-  Serial.print("Light Sensor "); Serial.print(" \t Digital Value: "); Serial.print(sensorValue[3]); 
-  Serial.print("    \t Analog Value: "); Serial.println(voltage[3]);
-
-  delay(2500);
+  delay(1000);
 }
